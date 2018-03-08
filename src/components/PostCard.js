09830e8a96cb2fc250bdbf82702720059d9c342e
Priyanka Luthra <priyanka@votigo.com>
2018-03-08 12:45:07 -0800

    hide button on individual post

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -14,19 +14,25 @@ class PostCard extends Component {
                     {this.props.post.title}
                     </Card.Header>
                     <Card.Meta>
-                    Friends of Elliot
                     </Card.Meta>
                     <Card.Description>
                     {this.props.post.body}
                     </Card.Description>
                 </Card.Content>
-                <Card.Content extra>
-                    <div className='ui buttons'>
-                    <Link to={`/post/${this.props.post.id}`}>
-                        <Button basic color='green'>View More</Button>
-                    </Link>
-                    </div>
-                </Card.Content>
+                {
+                !this.props.hideButton && (
+                    <Card.Content extra>
+                        <div className='ui buttons'>
+                            <Link to={`/post/${this.props.post.id}`}>
+                                <Button basic color='green'>View More</Button>
+                            </Link>
+                            </div>
+                    </Card.Content>
+                    )
+                }
+
+                    
+                
                 </Card>
             </Card.Group>
             
